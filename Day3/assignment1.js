3bae08d7560406928c9a83357a0a6b064f9c50b8
ksh <ksahoo524@gmail.com>
2021-08-05 00:25:41 +0900

    코드 추가

--- Day3/assignment1.js
+++ Day3/assignment1.js
@@ -1,5 +1,6 @@
+// 첫 풀이
 function solution(s){
-    let stack = [];
+    const stack = [];
     
     for(let x of s) {
         if(x === "(") stack.push(x);
@@ -8,4 +9,19 @@ function solution(s){
     }
 
     return stack.length ? false : true;
+}
+
+// 수정 풀이
+function solution(s){
+    let cnt = 0;
+
+    for(let x of s) {
+        if(x === "(") cnt++;
+        else {
+            if(cnt === 0) return false;
+            cnt--;
+        }
+    }
+
+    return cnt === 0;
 }
